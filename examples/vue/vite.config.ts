033c293a368db357ceb902f320e3e5caf4e841a3
xionkq <3030199850@qq.com>
2025-09-02 21:11:24 +0800

    ci: deploy to github page

--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/md-to-pdf/' : '/',
   // TODO: remove this
   server: {
     proxy: {
